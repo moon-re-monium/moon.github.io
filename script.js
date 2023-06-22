6bde6d6bc2f77068c4825157be6d79612bb806be
moon-re-monium <137092187+moon-re-monium@users.noreply.github.com>
2023-06-22 16:36:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,142 +1,141 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadText();
+  loadText();
+  countCharacters();
+  displayLastUpdated();
+
+  const textInput = document.getElementById('text-input');
+  textInput.addEventListener('input', function() {
     countCharacters();
-    displayLastUpdated();
+    updateLastUpdated();
+  });
 
-    const textInput = document.getElementById('text-input');
-    textInput.addEventListener('input', function() {
-        countCharacters();
-        updateLastUpdated();
-        updateLimitCount();
-    });
+  const limitInput = document.getElementById('limit-input');
+  limitInput.addEventListener('input', function() {
+    countCharacters();
+  });
 
-    const resetButton = document.getElementById('reset-button');
-    resetButton.addEventListener('click', function() {
-        resetCount();
-    });
+  const resetButton = document.getElementById('reset-button');
+  resetButton.addEventListener('click', function() {
+    resetCount();
+  });
 
-    const saveButton = document.getElementById('save-button');
-    saveButton.addEventListener('click', function() {
-        updateLastUpdated();
-        saveTextMemo();
-        saveText();
-        saveM();
-    });
+  const saveButton = document.getElementById('save-button');
+  saveButton.addEventListener('click', function() {
+    updateLastUpdated();
+    saveTextMemo();
+    saveText();
+  });
 
-    const copyButton = document.getElementById('copy-button');
-    copyButton.addEventListener('click', function() {
-        copyToClipboard();
-    });
+  const copyButton = document.getElementById('copy-button');
+  copyButton.addEventListener('click', function() {
+    copyToClipboard();
+  });
 
-    const textInputMemo = document.getElementById('kokomemo');
-    textInputMemo.addEventListener('input', function() {
-        saveTextMemo();
-        saveText();
-    });
-
-    const limitInput = document.getElementById('limit-input');
-    limitInput.addEventListener('input', function() {
-        updateLimitCount();
-    });
+  const textInputMemo = document.getElementById('kokomemo');
+  textInputMemo.addEventListener('input', function() {
+    saveTextMemo();
+    saveText();
+  });
 });
 
-
 function countCharacters() {
-    const text = document.getElementById('text-input').value;
-    const countWithSpaces = text.length;
-    const countWithoutSpaces = text.replace(/\s/g, '').length;
+  const text = document.getElementById('text-input').value;
+  const countWithSpaces = text.length;
+  const countWithoutSpaces = text.replace(/\s/g, '').length;
+  const limitInput = document.getElementById('limit-input').value;
+  const difference = limitInput - countWithoutSpaces;
 
-    document.getElementById('count-with-spaces').textContent = countWithSpaces;
+  const exceededText = document.getElementById('exceeded-text');
+  exceededText.textContent = '';
+
+  document.getElementById('count-with-spaces').textContent = countWithSpaces;
     document.getElementById('count-without-spaces').textContent = countWithoutSpaces;
 
-    saveText();
-    saveTextMemo();
+  if (limitInput === '' || limitInput === '0') {
+    return;
+  }
+
+  if (difference < 0) {
+    const span = document.createElement('span');
+    span.classList.add('limit-minus');
+    span.textContent = `+${Math.abs(difference)}`;
+    exceededText.appendChild(document.createTextNode("制限を超過しています："));
+    exceededText.appendChild(span);
+  } else {
+    const span = document.createElement('span');
+    span.classList.add('limit-plus');
+    span.textContent = `-${difference}`;
+    exceededText.appendChild(document.createTextNode("残り文字数："));
+    exceededText.appendChild(span);
+  }
+
+  saveText();
+  saveTextMemo();
 }
 
 function resetCount() {
-    document.getElementById('text-input').value = '';
-    document.getElementById('kokomemo').value = '';
-    document.getElementById('limit-input').value = '';
-    localStorage.clear();
-    countCharacters();
-    updateLimitCount();
+  document.getElementById('text-input').value = '';
+  document.getElementById('kokomemo').value = '';
+  document.getElementById('limit-input').value = '';
+  localStorage.clear();
+  countCharacters();
+  saveText();
+  saveTextMemo();
 }
 
 function copyToClipboard() {
   const text = document.getElementById('text-input').value;
   navigator.clipboard.writeText(text)
     .then(() => {
-      const copyButton = document.getElementById('copy-button');
-      copyButton.textContent = '✓';
-      setTimeout(() => {
-        copyButton.textContent = 'クリップボードにコピー';
-      }, 1000);
+      showCopyConfirmation();
     })
     .catch(() => {
       alert('テキストのクリップボードへのコピーに失敗しました');
     });
 }
 
+function showCopyConfirmation() {
+  const copyButton = document.getElementById('copy-button');
+  copyButton.textContent = '✓ クリップボードにコピー';
+
+  setTimeout(function() {
+    copyButton.textContent = 'クリップボードにコピー';
+  }, 1000);
+}
 
 function saveText() {
-    const text = document.getElementById('text-input').value;
-    localStorage.setItem('savedText', text);
+  const text = document.getElementById('text-input').value;
+  localStorage.setItem('savedText', text);
 }
 
 function saveTextMemo() {
-    const textMemo = document.getElementById('kokomemo').value;
-    localStorage.setItem('savedTextMemo', textMemo);
+  const textMemo = document.getElementById('kokomemo').value;
+  localStorage.setItem('savedTextMemo', textMemo);
 }
 
 function loadText() {
-    const savedText = localStorage.getItem('savedText');
-    if (savedText) {
-        document.getElementById('text-input').value = savedText;
-    }
+  const savedText = localStorage.getItem('savedText');
+  if (savedText) {
+    document.getElementById('text-input').value = savedText;
+  }
 
-    const savedTextMemo = localStorage.getItem('savedTextMemo');
-    if (savedTextMemo) {
-        document.getElementById('kokomemo').value = savedTextMemo;
-    }
+  const savedTextMemo = localStorage.getItem('savedTextMemo');
+  if (savedTextMemo) {
+    document.getElementById('kokomemo').value = savedTextMemo;
+  }
 }
 
 function displayLastUpdated() {
-    const timestamp = localStorage.getItem('lastUpdated');
-    if (timestamp) {
-        const date = new Date(parseInt(timestamp));
-        const formattedDate = date.toLocaleString('ja-JP');
-        document.getElementById('last-updated').textContent = `最終変更日時: ${formattedDate}`;
-    }
+  const timestamp = localStorage.getItem('lastUpdated');
+  if (timestamp) {
+    const date = new Date(parseInt(timestamp));
+    const formattedDate = date.toLocaleString('ja-JP');
+    document.getElementById('last-updated').textContent = `最終変更日時: ${formattedDate}`;
+  }
 }
 
 function updateLastUpdated() {
-    const timestamp = Date.now();
-    localStorage.setItem('lastUpdated', timestamp.toString());
-    displayLastUpdated();
-}
-
-function updateLimitCount() {
-    const limit = document.getElementById('limit-input').value;
-    const countWithoutSpaces = document.getElementById('count-without-spaces').textContent;
-
-    if (limit !== "") {
-        const difference = limit - countWithoutSpaces;
-
-        const exceededText = document.getElementById('exceeded-text');
-        exceededText.innerHTML = "";
-
-        if (difference < 0) {
-            const span = document.createElement('span');
-            span.classList.add('limit-minus');
-            span.textContent = `+${Math.abs(difference)}`;
-            exceededText.appendChild(document.createTextNode("制限を超過しています："));
-            exceededText.appendChild(span);
-        } else {
-            const span = document.createElement('span');
-            span.classList.add('limit-plus');
-            span.textContent = `-${difference}`;
-            exceededText.appendChild(document.createTextNode("残り文字数："));
-            exceededText.appendChild(span);
-        }
-    }
+  const timestamp = Date.now();
+  localStorage.setItem('lastUpdated', timestamp.toString());
+  displayLastUpdated();
 }
